7e0ffee60debe4141aa8b1890515ee69053f41e8
Fauzan Lubis <fauzanfauh45@gmail.com>
2024-04-07 20:53:49 +0700

    feat: remove the need of deps
    kembali fitri

--- src/commands/commands.rs
+++ src/commands/commands.rs
@@ -1,21 +1,16 @@
 use crate::resp::RespType;
-use phf::phf_map;
 
 use super::{hello::hello, ping::ping};
 
-static COMMANDS_LIST: phf::Map<&'static str, fn(&[RespType]) -> RespType> = phf_map! {
-    "PING" => ping,
-    "HELLO" => hello
-};
-
 pub fn handle_commands(command_arr: Vec<RespType>) -> RespType {
     if let Some(RespType::BulkString(command_name)) = command_arr.first() {
-        let command_handler = COMMANDS_LIST.get(&command_name);
+        let command_args = &command_arr[1..];
 
-        return match command_handler {
-            Some(handler) => handler(&command_arr[1..]),
+        match command_name.as_str() {
+            "PING" => ping(command_args),
+            "HELLO" => hello(command_args),
             _ => RespType::Error(format!("ERR unknown command '{}'", command_name)),
-        };
+        }
     } else {
         return RespType::Error(
             "WRONGTYPE wrong type, expected command name as a Bulk strings".into(),
